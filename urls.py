3664fe3c0192e8d9dd6b9a4ac93b4c6200d07949
Raja030110 <120628677+Raja030110@users.noreply.github.com>
2023-01-20 20:48:27 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,21 +1,25 @@
+"""forms URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path
-from formApp import views
-from django.contrib.admin.views.decorators import staff_member_required
+from django.urls import path,include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.index,name='index'),
-    path('book/',views.BookListView.as_view(),name='book'),
-    path('<int:pk>/',views.BookDetailView.as_view(),name='book_details'),
-    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('allbooks/', views.LoanedBooksByAdminListView.as_view(), name='all-borrowed'),
-    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
-
-
-
-
+    path('catalog/',include('formApp.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+    
 
 ]
-# LOGIN_REDIRECT_URL = '/'
